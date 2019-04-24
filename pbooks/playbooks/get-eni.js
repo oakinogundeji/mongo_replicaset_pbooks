4b92c45a570140ae4e8f336c5e757bcdcf0e03d7
telios <oakinogundeji@gmail.com>
2019-04-24 14:04:57 +0100

    made some mods to filter actual ENI data

---
+++ pbooks/playbooks/get-eni.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const input = process.argv[2];
+
+async function getEni() {
+  const eni = input.filter(eni => eni["description"].includes('node'));
+  const output = eni.map(eni => {
+    return {
+      "description": eni["description"],
+      "availability_zone": eni["availability_zone"],
+      "id": eni["id"],
+      "mac_address": eni["mac_address"],
+      "private_dns_name": eni["private_dns_name"],
+      "private_ip_address": eni["private_ip_address"],
+      "subnet_id": eni["subnet_id"]
+    }
+  });
+  return console.log(output);
+}
+
+getEni()
--- pbooks/playbooks/get-gateway.js
+++ pbooks/playbooks/get-gateway.js
@@ -8,19 +8,10 @@ const
 
 async function getGateway() {
   const {stdout, stderr, error} = await execFileAsync('ip', [`route`]);
-  //console.log('stdout');
-  //console.log(stdout);
-  //console.log(`typeof(stdout): ${typeof(stdout)}`);
   let stdoutArr = stdout.split('default via');
-  //console.log('stdoutArr');
-  //console.log(stdoutArr);
   let targetLine = stdoutArr[1].trim();
-  //console.log(`targetLine: ${targetLine}`);
   const targetLineArr = targetLine.split(' ');
-  //console.log('targetLineArr');
-  //console.log(targetLineArr);
   const gateway = targetLineArr[0];
-  //console.log(`gateway: ${gateway}`);
   return console.log(gateway);
   if(error){
     console.log('error');
