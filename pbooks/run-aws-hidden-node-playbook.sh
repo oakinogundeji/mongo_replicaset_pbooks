985e0596fb6d656b56b6ef413dbcddd1ea90f945
telios <oakinogundeji@gmail.com>
2019-04-29 15:56:49 +0100

    :package: deleted unneeded files, organized ubuntu into eni and vanilla dirs

--- pbooks/run-aws-hidden-node-playbook.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-ansible-playbook -i /home/ec2-user/playbooks/pbooks/ec2.py -c local /home/ec2-user/playbooks/pbooks/awslinux/hidden-node.yml --extra-vars "BACKUP_BUCKET=tss-nazar-mongod-test EMAIL_SENDER=tss@tssdevs.com"
